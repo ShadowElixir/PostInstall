6ba5c8ea5f6c7fd94bb99ad10bef4e20b0506a8a
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2026-02-28 15:05:03 +0200

    Create Romanitho.Winget-AutoUpdate-installed.ps1

---
+++ files/Romanitho.Winget-AutoUpdate-installed.ps1
@@ -0,0 +1,3 @@
+# Allows automatic updates for apps that wau blocks for some reason.
+Rename-Item "C:\Program Files\Winget-AutoUpdate\config\default_excluded_apps.txt" "C:\Program Files\Winget-AutoUpdate\config\default_excluded_apps_backup.txt"
+echo "" "C:\Program Files\Winget-AutoUpdate\config\default_excluded_apps.txt"
