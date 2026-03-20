d0c959a6f59a1707d93c0003e02dd3d05514a627
ShadowElixir <47082432+ShadowElixir@users.noreply.github.com>
2026-03-20 11:01:21 +0200

    Update confirmation.ps1

--- confirmation.ps1
+++ confirmation.ps1
@@ -1,9 +1,10 @@
-Write-Host "This script will:" -F Yellow
+Write-Host "This script will:"
 Write-Host "1. Activate Windows (Using MAS)"
 Write-Host "2. Install essential programs using winget (Type 'i' for info)"
 Write-Host "3. Install a basic powershell profile with fastfetch"
 Write-Host "4. Debloat Windows (using Chris Titus' debloat script)"
 Write-Host "WARNING: This will delete all MS Store (UWP) apps, and MS Edge!" -F Red
+Write-Host "WARNING: Make sure you've fully updated windows before running!" -F Red
 
 $choice = Read-Host "Do you want to proceed? (y/n/i)"
 
